c3a887bdb481b979926083e6c2c251ad4e2b6e53
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-12-21 21:42:10 -0600

    fix(global): add sql3 and fix som bug

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -6,6 +6,7 @@ const api = {}
 // Use `contextBridge` APIs to expose Electron APIs to
 // renderer only if context isolation is enabled, otherwise
 // just add to the DOM global.
+
 if (process.contextIsolated) {
   try {
     contextBridge.exposeInMainWorld('electron', electronAPI)
