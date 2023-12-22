6fe8f5d413c049484d031e42577aec1c17e82a8f
Vichea <48352653+vicheanath@users.noreply.github.com>
2023-12-22 14:19:46 -0600

    feat(global): add Word Detail Screen & React Router Dom
    Add UI with font-style in Detail word screen

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -3,6 +3,7 @@ import Headers from './components/Header'
 import { DataContextProvider } from './contexts/DataContextProvider'
 import HomeScreen from './screens/HomeScreen'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import WordDetailScreen from './screens/WordDetailScreen'
 
 function App(): JSX.Element {
   return (
@@ -13,6 +14,7 @@ function App(): JSX.Element {
           <FixedGridPanel>
             <Routes>
               <Route path="/" element={<HomeScreen />} />
+              <Route path="/word/:id" element={<WordDetailScreen />} />
             </Routes>
           </FixedGridPanel>
         </GridPanel>
---
+++ src/renderer/src/screens/WordDetailScreen.tsx
@@ -0,0 +1,65 @@
+import { Button } from '@renderer/components/Button'
+import { SearchBar } from '@renderer/components/SearchBar'
+import { Word, WordControllerImpl } from '@renderer/libs/Word'
+import { FC, useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { SolidHome } from '@renderer/components/icons'
+
+const WordDetailScreen: FC = (): JSX.Element => {
+  const { id } = useParams<{ id: string }>()
+  const navigate = useNavigate()
+  const [loading, setLoading] = useState<boolean>(false)
+  const [word, setWord] = useState<Word>()
+  useEffect(() => {
+    setLoading(true)
+    const getWord = WordControllerImpl.find(new Number(id).valueOf())
+    getWord.then((res) => {
+      setWord(res)
+      setLoading(false)
+    })
+  }, [])
+  return (
+    <div className="container mx-auto">
+      <div className="flex flex-row">
+        <Button
+          icon={<SolidHome />}
+          className="ml-4"
+          onClick={() => {
+            navigate('/')
+          }}
+        >
+          Home
+        </Button>
+      </div>
+      <div className="flex flex-col">
+        <div className="py-2 inline-block min-w-full">
+          <div className="shadow overflow-hidden">
+            <div className="flex flex-col gap-2">
+              <div className="flex flex-col bg-primary-800 rounded-8">
+                {loading ? (
+                  <div>Loading...</div>
+                ) : (
+                  <>
+                    <div className="px-6 py-4">
+                      <h1 className="text-lg text-gray-900">{word?.word}</h1>
+                    </div>
+                    <div className="px-6 py-4">
+                      <div
+                        className="text-lg text-gray-900"
+                        dangerouslySetInnerHTML={{
+                          __html: word?.details.trim() ?? ''
+                        }}
+                      />
+                    </div>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default WordDetailScreen
